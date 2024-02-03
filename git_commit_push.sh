ceefcd2a0f37a75c51f107cca500a53778960ff7
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-04 02:34:56 +0300

    style: Add custom styles and overrides
    Integrate custom styles for various components, including a base font family, utility classes for handling focus rings, and styles for a right-side menu. Import styles for Liveblocks React Comments.

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,9 +5,9 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-style: Configure Tailwind CSS with custom theme and plugins
+style: Add custom styles and overrides
 
-Configure Tailwind CSS with a custom theme, including container settings, color extensions, and custom animations. Add relevant content paths for Tailwind to analyze.
+Integrate custom styles for various components, including a base font family, utility classes for handling focus rings, and styles for a right-side menu. Import styles for Liveblocks React Comments.
 
 "
 # Push changes to the remote repository

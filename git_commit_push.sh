cdc37b7990cd60e83ea3de8c81bd76d55b796f83
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-04 03:01:01 +0300

    feat: adding public folder

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,10 +5,7 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Define TypeScript interfaces and types for various components and functionalities
-
-Introduce TypeScript interfaces and types for managing cursor state, reactions, shape data, attributes, active elements, fabric objects, and various props across different components.
-
+feat: adding public folder
 "
 # Push changes to the remote repository
 git push origin HEAD

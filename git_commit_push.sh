fffe9ef658a31376b1844e99b8af1e3aabe81214
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-09 01:34:42 +0300

    feat: Implement Room component for Liveblocks integration
    - Created a Room component for Liveblocks integration.
    - Utilized RoomProvider from liveblocks.config to set up the room with initial presence and storage configurations.
    - Used ClientSideSuspense to handle suspense while loading.
    - Added Loader component as a fallback during suspense.
    
    This commit introduces a Room component to facilitate Liveblocks integration. It sets up the room with initial presence and storage configurations and handles suspense with a Loader component during data fetching.

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,7 +5,15 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-remove readme text
+feat: Implement Room component for Liveblocks integration
+
+- Created a Room component for Liveblocks integration.
+- Utilized RoomProvider from liveblocks.config to set up the room with initial presence and storage configurations.
+- Used ClientSideSuspense to handle suspense while loading.
+- Added Loader component as a fallback during suspense.
+
+This commit introduces a Room component to facilitate Liveblocks integration. It sets up the room with initial presence and storage configurations and handles suspense with a Loader component during data fetching.
+
 "
 # Push changes to the remote repository
 git push origin HEAD

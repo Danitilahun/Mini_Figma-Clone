c954713f9ee244e660363dba4365f50a36f22cdc
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-09 01:20:31 +0300

    remove readme text

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,14 +5,7 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-chore: Update Next.js configuration
-
-- Added external dependencies 'utf-8-validate', 'bufferutil', and 'canvas' to be bundled as commonjs modules.
-- Configured remote patterns for images to include 'liveblocks.io'.
-- Set TypeScript to ignore build errors for improved development experience.
-
-This commit updates the Next.js configuration to optimize external dependency handling, specify remote patterns for images, and adjust TypeScript settings for better development workflow.
-
+remove readme text
 "
 # Push changes to the remote repository
 git push origin HEAD
